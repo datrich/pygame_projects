f8c4e84644d002a8419855134f84ae6e8315b35b
Bùi Minh Tuấn Đạt <86585127+datrich@users.noreply.github.com>
2024-10-24 03:07:32 +0700

    Update snake_pygame.py
    i change my method to approach the game, i use list to store the move of snake and change the color of this move and it is game changer!!!

--- snake_pygame.py
+++ snake_pygame.py
@@ -2,18 +2,19 @@ import pygame as pg
 from numpy import random
 pg.init()
 pg.font.init()
-pos_ball = tuple(random.randint(600,size = 2))
+
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 run = True
 clock = pg.time.Clock()
 
 hit = 0
-
+white = (255, 255, 255)
+black = (0, 0, 0)
+blue_s = (80, 80, 255)
 font = pg.font.Font('bebas.ttf', 24)
-nofi_font = pg.font.Font('bebas.ttf', 70)
-time = font.render('', True, (255,255,255))
-score = font.render('score: 0', True, (0,0,0))
+time = font.render('', True, white)
+score = font.render('score: 0', True, black)
 timeRect = time.get_rect()
 scoreRect = score.get_rect()
 timeRect.center = (30, 20)
@@ -23,90 +24,103 @@ pg.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT)) # just windows popup!
 screen = pg.display.get_surface()
 surface = pg.Surface((800, 560))
 
+pos_ball_x = random.randint(800)//10*10
+pos_ball_y = random.randint(560)//10*10
 a = 0
 b = pg.time.get_ticks()
-x = 0
-y = 0
-left  = False
-right  = False
-up  = False
-down  = False
+x = random.randint(800)//10*10
+y = random.randint(560)//10*10
+k = 1
+direct = ''
+snake_pos = [[0,0]]
+
 while run:
-	r, g, b = 255, 255, 255
+
 	if (pg.time.get_ticks()//1000-a*60-5) == 60:
 		a +=1
-	time = font.render('TOTAL TIME:   '+str(a)+':'+str(pg.time.get_ticks()//1000-a*60), True, (255,255,255))
-	screen.fill("black")
+	time = font.render('TOTAL TIME:   '+str(a)+':'+str(pg.time.get_ticks()//1000-a*60), True, black)
+	screen.fill((255,128,0))
 	screen.blit(time,timeRect)
+
+	score = font.render('score: '+str(hit), True, black)
+	screen.blit(score,scoreRect)
 
 	for event in pg.event.get():
 		if event.type == pg.QUIT:
 			run = False
+
 	ar = pg.PixelArray(surface)
-	ar[x:x+9, y:y+9] = (r, g, b)
+
+	ar[:,:] = black
+	key = pg.key.get_pressed()
+
+	if key[pg.K_LEFT] == True and direct != 'right':
+		direct = 'left'
+	elif key[pg.K_RIGHT] == True and direct != 'left':
+		direct = 'right'
+	elif key[pg.K_UP] == True and direct != 'down':
+		direct = 'up'
+	elif key[pg.K_DOWN] == True and direct != 'up':
+		direct = 'down'
+
+	if direct == 'left':
+		x -= 10
+		if x < 0:
+			break
+		if [x,y] in snake_pos:
+			break
+
+	elif direct == 'right':
+		x += 10
+		if x > 800:
+			break
+		if [x,y] in snake_pos:
+			break
+
+	elif direct == 'up':
+		y -= 10
+		if y < 0:
+			break
+		if [x,y] in snake_pos:
+			break
+
+	elif direct == 'down':
+		y += 10
+		if y > 560:
+			break
+		if [x,y] in snake_pos:
+			break
+
+	snake_pos.append([x,y])
+	if len(snake_pos) != k:
+			snake_pos.pop(0)
+	for i in range(len(snake_pos)):
+		ar[snake_pos[i][0]:snake_pos[i][0]+9, snake_pos[i][1]:snake_pos[i][1]+9] = white
+	ar[pos_ball_x:pos_ball_x+9,pos_ball_y:pos_ball_y+9] = blue_s
 	del ar
 	screen.blit (surface, (0, 40))
-	ar = pg.PixelArray(surface)
-
-	key = pg.key.get_pressed()
-	r, g, b = 255, 255, 255
-	if key[pg.K_LEFT] == True and right == False:
-		left  = True
-		right  = False
-		up  = False
-		down  = False
-		l_time = pg.time.get_ticks()
-	elif key[pg.K_RIGHT] == True and left == False:
-		left  = False
-		right  = True
-		up  = False
-		down  = False
-		l_time = pg.time.get_ticks()
-	elif key[pg.K_UP] == True and down == False:
-		left  = False
-		right  = False
-		up  = True
-		down  = False
-		l_time = pg.time.get_ticks()
-	elif key[pg.K_DOWN] == True and up == False:
-		left  = False
-		right  = False
-		up  = False
-		down  = True
-		l_time = pg.time.get_ticks()
-
-	if left == True and pg.time.get_ticks()-l_time > 100:
-		x -= 10
-		ar[:,:] = (0, 0, 0)
-		ar[x:x+9, y:y+9] = (r, g, b)
-		del ar
-		screen.blit (surface, (0, 40))
-		l_time = pg.time.get_ticks()
-
-	elif right == True and pg.time.get_ticks()-l_time > 100:
-		x += 10
-		ar[:,:] = (0, 0, 0)
-		ar[x:x+9, y:y+9] = (r, g, b)
-		del ar
-		screen.blit (surface, (0, 40))
-		l_time = pg.time.get_ticks()
-
-	elif up == True and pg.time.get_ticks()-l_time > 100:
-		y -= 10
-		ar[:,:] = (0, 0, 0)
-		ar[x:x+9, y:y+9] = (r, g, b)
-		del ar
-		screen.blit (surface, (0, 40))
-		l_time = pg.time.get_ticks()
-
-	elif down == True and pg.time.get_ticks()-l_time > 100:
-		y += 10
-		ar[:,:] = (0, 0, 0)
-		ar[x:x+9, y:y+9] = (r, g, b)
-		del ar
-		screen.blit (surface, (0, 40))
-		l_time = pg.time.get_ticks()
+	# eat
+	if x == pos_ball_x and y == pos_ball_y:
+		pos_ball_x = random.randint(800)//10*10
+		pos_ball_y = random.randint(560)//10*10
+		k += 1
+		hit += 1
 
 	pg.display.flip()
-	clock.tick(60)
+	clock.tick(10)
+
+font1 = pg.font.Font('bebas.ttf', 72)
+score1 =  font1.render('score: '+str(hit), True, black)
+scoreRect1 = score1.get_rect()
+scoreRect1.center = (400,220)
+
+run = True
+c = pg.time.get_ticks() + 5000
+while pg.time.get_ticks() < c and run:
+	screen.blit(score1,scoreRect1)
+	pg.display.flip()
+	screen.fill((255,128,0))
+	for event in pg.event.get():
+		if event.type == pg.QUIT:
+			run = False
 pg.quit()
